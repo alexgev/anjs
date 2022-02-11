0a58163d80611155aaeeab335b39a3a8f76d76e8
Alexey Gevorkyan <gevorkyan.alexey@gmail.com>
2022-02-11 23:23:44 +0200

    feat: composition

--- src/4/brandedType.ts
+++ src/4/brandedType.ts
@@ -1,28 +1,28 @@
-type UserEmail = string & {readonly UserEmail: unique symbol};
+// type UserEmail = string & {readonly UserEmail: unique symbol};
 
-const validate = (value: string) => {
-  return value && /^[\d\w\.\-]+@itkachalka.ru$/.test(value);
-}
+// const validate = (value: string) => {
+//   return value && /^[\d\w\.\-]+@itkachalka.ru$/.test(value);
+// }
 
-const UserEmail = (value: string) => {
-  if (!validate(value)) {
-    throw new Error('Invalid user email');
-  }
-  return value as UserEmail
-}
+// const UserEmail = (value: string) => {
+//   if (!validate(value)) {
+//     throw new Error('Invalid user email');
+//   }
+//   return value as UserEmail
+// }
 
-type User = {
-  id: string,
-  email: UserEmail
-}
+// type User = {
+//   id: string,
+//   email: UserEmail
+// }
 
-const user: User = {
-  id: '1',
-  email: UserEmail('gevorkyan.alexey@itkachalka.ru')
-}
+// const user: User = {
+//   id: '1',
+//   email: UserEmail('gevorkyan.alexey@itkachalka.ru')
+// }
 
 
-const fu = (id: string, email: UserEmail) => {
-  console.log('success');
-};
-fu(user.id, user.email);
+// const fu = (id: string, email: UserEmail) => {
+//   console.log('success');
+// };
+// fu(user.id, user.email);
--- src/4/shop.ts
+++ src/4/shop.ts
@@ -1,153 +1,293 @@
-const shop = () => {
-  const validate = (value: any) => {
-    return value;
+const validate = (value: any) => {
+  return value;
+}
+
+type FilmId = number & {readonly FilmId: unique symbol};
+const FilmId = (id: number) => {
+  if (!validate(id)) {
+    throw new Error('Validate');
   }
+  return id as FilmId;
+}
 
-  
-
-  type ContentId = number & {readonly ContentId: unique symbol};
-  const ContentId = (id: number) => {
-    if (!validate(id)) {
-      throw new Error('Validate');
-    }
-    return id as ContentId;
+type SerialId = number & {readonly SerialId: unique symbol};
+const SerialId = (id: number) => {
+  if (!validate(id)) {
+    throw new Error('Validate');
   }
+  return id as SerialId;
+}
 
-  type UserId = number & {readonly UserId: unique symbol};
-  const UserId = (id: number) => {
-    if (!validate(id)) {
-      throw new Error('Validate');
-    }
-    return id as UserId;
+type UserId = number & {readonly UserId: unique symbol};
+const UserId = (id: number) => {
+  if (!validate(id)) {
+    throw new Error('Validate');
   }
+  return id as UserId;
+}
 
-  type UserEmail = string & {readonly UserEmail: unique symbol};
-  const UserEmail = (email: string) => {
-    if (!validate(email)) {
-      throw new Error('Validate');
-    }
-    return email as UserEmail;
+type UserEmail = string & {readonly UserEmail: unique symbol};
+const UserEmail = (email: string) => {
+  if (!validate(email)) {
+    throw new Error('Validate');
   }
+  return email as UserEmail;
+}
 
-  type ContentVideo = Buffer & {readonly ContentVideo: unique symbol};
-  const ContentVideo = (buffer: Buffer) => {
-    if (!validate(buffer)) {
-      throw new Error('Validate');
-    }
-    return buffer as ContentVideo;
+type ContentVideo = Buffer & {readonly ContentVideo: unique symbol};
+const ContentVideo = (buffer: Buffer) => {
+  if (!validate(buffer)) {
+    throw new Error('Validate');
   }
+  return buffer as ContentVideo;
+}
 
-  // models
+/**
+ * Фукнциональная композиция данных
+ */
 
-  type Content = {
-    id: ContentId,
-    name: string,
+const functionalComposition = () => {
+
+  type ContentWithVideo = {
     video: ContentVideo
   }
 
-  type Film = Content & {
-    duration: number
+  type ProductsOwner = {
+    products: Product[]
   }
 
-  type Serial = Content & {
-    countOfSeries: number
+  type Film = ContentWithVideo & {
+    id: FilmId,
+    duration: number
+    name: string,
   }
+
+  type Serial = ContentWithVideo & {
+    id: SerialId,
+    countOfSeries: number,
+    name: string,
+  }
+
+  type Product = Film | Serial;
   
-  type User = {
+  type User = ProductsOwner & {
     id: UserId,
     email: UserEmail,
-    contents: Content[]
   }
 
-  // test db
-
-  const films: Film[] = [{
-    id: ContentId(1),
-    name: 'film 1',
-    video: ContentVideo(Buffer.alloc(2)),
-    duration: 1
-  }]
-
-  const serials: Serial[] = [{
-    id: ContentId(2),
-    name: 'serial 1',
-    video: ContentVideo(Buffer.alloc(2)),
-    countOfSeries: 2
-  }]
-  
-  const contents: Content[] = [...serials, ...films];
-
-  const users: User[] = [{
-    id: UserId(1),
-    email: UserEmail('asdf'),
-    contents
-  }, {
-    id: UserId(2),
-    email: UserEmail('awerafs'),
-    contents: []
-  }];
-
-  // helpers
-
-  const findContentById = (id: ContentId) => {
-    const content = contents.find(film => film.id === id);
-    if (!content) throw new Error('not found content');
-    return content;
-  }
-
-  const findUserById = (id: UserId) => {
-    const user = users.find(user => user.id === id);
-    if (!user) throw new Error('not found user');
-    return user;
-  }
-
-  const removeContentFromArray = (contents: Content[], filmId: ContentId) => {
-    const index = contents.findIndex(film => film.id === filmId);
-    if (~index) {
-      return contents.splice(index, 1)[0];
-    } else {
-      throw new Error('Not found film in array');
-    }
+  type Guest = ProductsOwner & {
+    id: UserId
   }
 
   // Business logic
 
-  const addVideoToContent = (id: ContentId, video: ContentVideo) => {
-    const content = findContentById(id);
-    content.video = video;
-    return content;
+  const addVideoToProduct = (contentWithVideo: ContentWithVideo, video: ContentVideo) => {
+    contentWithVideo.video = video;
+    return contentWithVideo;
   }
 
-  const buyContent = (userId: UserId, contentId: ContentId) => {
-    const [
-      user,
-      film
-    ] = [
-      findUserById(userId),
-      findContentById(contentId)
-    ]
-    user.contents.push(film);
+  const buyProduct = (user: ProductsOwner, product: Product) => {
+    user.products.push(product);
   }
 
-  const sellContentToAnotherUser = (sellerId: UserId, buyerId: UserId, contentId: ContentId) => {
-    const [
-      seller,
-      buyer,
-      content
-    ] = [
-      findUserById(sellerId),
-      findUserById(buyerId),
-      findContentById(contentId)
-    ];
-    buyer.contents.push(removeContentFromArray(seller.contents, contentId));
+  const sellProductToAnotherUser = (seller: ProductsOwner, buyer: ProductsOwner, product: Product) => {
+    const idx = seller.products.findIndex(product => product.id === product.id);
+    if (!~idx) throw new Error('not found serial');
+    buyer.products.push(seller.products.splice(idx, 1)[0]);
+    return product;
   }
-
-
-  // Business logic execution
-
-  addVideoToContent(ContentId(1), ContentVideo(Buffer.alloc(3)))
-  buyContent(UserId(2), ContentId(2));
-  sellContentToAnotherUser(UserId(1), UserId(2), ContentId(1));
 }
 
-shop();
+functionalComposition();
+
+/**
+ * Композиция на Union Type
+ */
+
+const composionWithUnionTypes = () => {
+
+  type Film = {
+    id: FilmId,
+    duration: number
+    name: string,
+    video: ContentVideo
+  }
+
+  type Serial = {
+    id: SerialId,
+    countOfSeries: number,
+    name: string,
+    video: ContentVideo
+  }
+
+  type Product = Film | Serial;
+
+  
+  type User = {
+    id: UserId,
+    email: UserEmail,
+    products: Product[];
+  }
+
+  type Guest = {
+    id: UserId,
+    products: Product[];
+  }
+
+  type ProductsOwner = User | Guest;
+
+  // Business logic
+
+  const addVideoToProduct = (contentWithVideo: Product, video: ContentVideo) => {
+    contentWithVideo.video = video;
+    return contentWithVideo;
+  }
+
+  const buyProduct = (user: ProductsOwner, product: Product) => {
+    user.products.push(product);
+  }
+
+  const sellProductToAnotherUser = (seller: ProductsOwner, buyer: ProductsOwner, product: Product) => {
+    const idx = seller.products.findIndex(product => product.id === product.id);
+    if (!~idx) throw new Error('not found serial');
+    buyer.products.push(seller.products.splice(idx, 1)[0]);
+    return product;
+  }
+}
+
+composionWithUnionTypes();
+
+
+
+/**
+ * Композиция на интерфейсах
+ */
+
+const composionWithInterfaces = () => {
+
+  type Film = {
+    id: FilmId,
+    duration: number
+    name: string,
+    video: ContentVideo
+  }
+
+  type Serial = {
+    id: SerialId,
+    countOfSeries: number,
+    name: string,
+    video: ContentVideo
+  }
+
+  type ProductWithVideo = {
+    video: ContentVideo
+  }
+
+  type Product = {
+    id: number,
+    name: string;
+  }
+
+
+
+  
+  type User = {
+    id: UserId,
+    email: UserEmail,
+    products: Product[];
+  }
+
+  type Guest = {
+    id: UserId,
+    products: Product[];
+  }
+
+  type ProductsOwner = {
+    products: Product[]
+  };
+
+  // Business logic
+
+  const addVideoToProduct = (contentWithVideo: ProductWithVideo, video: ContentVideo) => {
+    contentWithVideo.video = video;
+    return contentWithVideo;
+  }
+
+  const buyProduct = (user: ProductsOwner, product: Product) => {
+    user.products.push(product);
+  }
+
+  const sellProductToAnotherUser = (seller: ProductsOwner, buyer: ProductsOwner, product: Product) => {
+    const idx = seller.products.findIndex(product => product.id === product.id);
+    if (!~idx) throw new Error('not found serial');
+    buyer.products.push(seller.products.splice(idx, 1)[0]);
+    return product;
+  }
+}
+
+composionWithInterfaces();
+
+
+/**
+ * Композиция на интерфейсах
+ */
+
+ const behaviorComposition = () => {
+
+  type User = {
+    email: string,
+    products: Product[],
+  }
+
+
+  type Film = {
+    name: string,
+    hello: string
+  }
+
+  type Serial = {
+    name: string,
+    bla: number,
+  }
+
+  type Product =  Film | Serial;
+
+
+  type BuyBehavior = (user: User, product: Product) => Product;
+
+  const buyProduct = (user: User, product: Product, behavour: BuyBehavior) => {
+    return behavour(user, product);
+  }
+
+  const buyFilm = (user: User, film: Film) => {
+    console.log('buy film');
+    user.products.push(film);
+    return film;
+  }
+
+  const buySerial = (user: User, serial: Serial) => {
+    console.log('buy serial');
+    user.products.push(serial);
+    return serial;
+  }
+
+  const user: User = {
+    email: 'asdf',
+    products: []
+  }
+
+  const film: Film = {
+    name: 'asdf',
+    hello: 'asdf'
+  }
+  const serial: Serial = {
+    bla: 1,
+    name: 'asdf'
+  }
+
+  buyProduct(user, film, buyFilm);
+  buyProduct(user, serial, buySerial);
+
+}
+
+behaviorComposition();
